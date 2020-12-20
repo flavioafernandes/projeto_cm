bb86124ce440e7be02ba018181566da1765873f3
FabioFerr <fabioferreira120@hotmail.com>
2020-12-20 22:19:38 +0000

    splash screen

---
+++ app/src/main/java/com/example/projectcm/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.projectcm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int Splash_Time_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //findViewById(R.id.intro_img).setAlpha(0);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()  {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        },Splash_Time_OUT);
+    }
+}
